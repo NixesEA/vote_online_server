653b8f596acc2c17708823e42206e8cf1a0f3cd6
Ermakov Anton <anton.ermakov@pushapp.ru>
2019-10-16 22:17:34 +0300

    v3

--- src/main/java/io/vertx/example/HelloWorldVerticle.kt
+++ src/main/java/io/vertx/example/HelloWorldVerticle.kt
@@ -1,15 +1,30 @@
-package io.vertx.example;
+package io.vertx.example
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpServerRequest
+import io.vertx.ext.web.Router
 
-public class HelloWorldVerticle extends AbstractVerticle {
+class HelloWorldVerticle : AbstractVerticle() {
 
-  @Override
-  public void start() throws Exception {
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello Niger!"))
-    .listen(
-        Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"));
-  }
+    private val router = Router.router(vertx).apply {
+        /**
+         * Welcome handler.
+         */
+        get("/").handler { ctx ->
+            ctx.response().end("Welcome in MainVerticle!")
+        }
+
+        get("/check").handler { ctx ->
+            ctx.response().end("Welcome in check!")
+        }
+    }
+
+    @Throws(Exception::class)
+    override fun start() {
+        vertx.createHttpServer()
+                .requestHandler { router.accept(it) }
+                .listen(
+                        Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"))
+
+    }
 }
-

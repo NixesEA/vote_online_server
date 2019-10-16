092e18fc4f5d6ed544227c8ad6ece99530e55b80
Ermakov Anton <anton.ermakov@pushapp.ru>
2019-10-16 22:25:55 +0300

    testung

--- src/main/java/io/vertx/example/EntryPoint.java
+++ src/main/java/io/vertx/example/EntryPoint.java
@@ -4,10 +4,10 @@ import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 
 public class EntryPoint {
-    private static Vertx vertx = Vertx.vertx();
-    public static Router router = Router.router(vertx);
-
-    public static void main(String[] args) {
-        vertx.deployVerticle(new HelloWorldVerticle());
-    }
+//    private static Vertx vertx = Vertx.vertx();
+//    public static Router router = Router.router(vertx);
+//
+//    public static void main(String[] args) {
+//        vertx.deployVerticle(new HelloWorldVerticle());
+//    }
 }
